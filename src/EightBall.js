0769e413ecfd179f20cd0d2f43e55cf5e3e8aa09
John Morgan <johnhmorgan@outlook.com>
2021-03-23 08:43:34 +0000

    initial commit

---
+++ src/EightBall.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+// import answers from './answers';
+import './EightBall.css';
+
+
+const EightBall = ({answers}) => {
+
+    const genIdx = () => Math.floor(Math.random() * answers.length);
+
+    const [idx, setIdx] = useState(genIdx());
+    let myColor = answers[idx].color;
+    let myMsg = answers[idx].msg;
+
+    return (
+        <div className="EightBall" style={{backgroundColor: myColor}}
+        onClick={() => setIdx(genIdx)}
+        >
+            <br></br>
+            <br></br>
+            <br></br>
+            <h1 className='EightBall-question'>{myMsg}</h1>
+        </div>
+    )
+
+}
+
+
+export default EightBall;
